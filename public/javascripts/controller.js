cfb94e1cf25db0ad939da059dc677a5fe0303e57
HARIHARAN <hariharanwebmail@gmail.com>
2014-03-30 12:54:51 +0530

    Integrating leap motion device leapjs.

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,5 +1,36 @@
 var HandLeapApp = angular.module('HandLeapApp', []);
- 
+
 HandLeapApp.controller('HandLeapController', function ($scope) {
-  $scope.test = "it works.!!!!";
+
+	$scope.controller ={};
+	$scope.connectionStatus = "NOTHING GREAT.. Connect the device";
+
+	var init = function(){
+		$scope.controller = new Leap.Controller();
+
+		$scope.controller.on('connect', function() {
+			console.log("Connected");
+		  changeStatus("Successfully connected to device");;
+		});
+
+		$scope.controller.on('deviceConnected', function() {
+			console.log("device Connected");
+		  changeStatus("A Leap device has been connected.");
+		});
+
+		$scope.controller.on('deviceDisconnected', function() {
+			console.log("device disconnected");
+		  changeStatus("A Leap device has been disconnected.");
+		});
+
+		$scope.controller.connect();
+	}
+
+	var changeStatus = function(status){
+    $scope.$apply(function(){
+      $scope.connectionStatus = status;
+    });
+  }
+
+	init();
 });
