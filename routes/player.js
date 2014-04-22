58154657ee0531bb8f9e0fedf2bd0e57fb196e69
harihat <harihat@INharihat-2.local>
2014-04-22 11:27:26 +0530

    Commands to actual itunes player.

--- routes/player.js
+++ routes/player.js
@@ -1,15 +1,64 @@
+var applescript = require("applescript");
+
+
+var nextTrackCommand = 'tell application "iTunes" \n next track \nend tell';
+var previousTrackCommand = 'tell application "iTunes" \n previous track \nend tell';
+var playPauseCommand = 'tell application "iTunes" \n playpause \nend tell';
+
 exports.next = function(req,res){
-    res.send("Success next");
+  applescript.execString(nextTrackCommand, function(err, rtn){
+    if(!err){
+      res.send("Success next");
+    }else{
+      console.log(err);
+      res.send("Next song failed");
+    }
+  });
 }
 
 exports.prev = function(req,res){
-    res.send("Success prev");
+  applescript.execString(previousTrackCommand, function(err, rtn){
+    if(!err){
+      res.send("Success prev");
+    }else{
+      console.log(err);
+      res.send("Next song failed");
+    }
+  });
 }
 
 exports.play = function(req,res){
-    res.send("Success stop");
+  applescript.execString(playPauseCommand, function(err, rtn){
+    if(!err){
+      res.send("Success play");
+    }else{
+      console.log(err);
+      res.send("Next song failed");
+    }
+  });
 }
 
 exports.changeVolume = function(req, res) {
+
+
     res.send("Volume set at "+req.query.level);
 }
+
+
+/*
+var applescript = require("applescript");
+
+// Very basic AppleScript command. Returns the song name of each
+// currently selected track in iTunes as an 'Array' of 'String's.
+var script = 'tell application "iTunes" to get name of selection';
+
+applescript.execString(script, function(err, rtn) {
+  if (err) {
+    // Something went wrong!
+  }
+  if (Array.isArray(rtn)) {
+    rtn.forEach(function(songName) {
+      console.log(songName);
+    });
+  }
+});*/
